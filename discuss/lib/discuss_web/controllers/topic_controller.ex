47644ef29989de81a91a056e37e7b14b20471465
alexis lenoir <alexis59495@gmail.com>
2021-07-20 14:56:17 -0300

    Start with phoenix

---
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,25 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  alias Discuss.Topics.Topic
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn,  %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+  end
+end
